414d06ef889c91f06fb725014bf86e6ea73df682
Farshad <farshad_asgharzade@hotmail.com>
2020-12-22 11:05:15 +0330

    fix global variable

--- src/Optimization/Critical/CSS/Purge.php
+++ src/Optimization/Critical/CSS/Purge.php
@@ -1,2 +1,2 @@
 <?php
- namespace Optimization\Critical\CSS; use Optimization\Critical\Purge as BaseClass; class Purge extends BaseClass { public function __construct() { goto ywJXm; ywJXm: $this->id = "\143\x72\151\164\x69\143\x61\x6c\55\143\x73\163"; goto ASam0; ASam0: $this->paths = [Util::getFullPath()]; goto cv9wu; cv9wu: parent::__construct(); goto EJvdU; EJvdU: } }
+ namespace Optimization\Critical\CSS; use Optimization\Critical\Purge as BaseClass; class Purge extends BaseClass { public function __construct() { goto RPuso; RPuso: $this->id = "\x63\x72\151\x74\x69\143\141\154\x2d\143\x73\x73"; goto qLAnY; ybJLE: parent::__construct(); goto doCw6; qLAnY: $this->paths = [Util::getFullPath()]; goto ybJLE; doCw6: } }
--- src/Optimization/Purge/File.php
+++ src/Optimization/Purge/File.php
@@ -1,2 +1,2 @@
 <?php
- namespace Optimization\Purge; class File extends Common { public function __construct(?string $GuTuU = null) { parent::__construct($GuTuU); } }
+ namespace Optimization\Purge; class File extends Common { public function __construct(?string $path = null) { parent::__construct($path); } }
