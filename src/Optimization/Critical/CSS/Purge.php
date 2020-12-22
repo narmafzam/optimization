5f42f8fdd0f3925a1e5ba41e9cb1318fb0195398
Farshad <farshad_asgharzade@hotmail.com>
2020-12-22 21:22:51 +0330

    update code

--- src/Optimization/Critical/CSS/Purge.php
+++ src/Optimization/Critical/CSS/Purge.php
@@ -1,2 +1,2 @@
 <?php
- namespace Optimization\Critical\CSS; use Optimization\Critical\Purge as BaseClass; class Purge extends BaseClass { public function __construct() { goto RPuso; RPuso: $this->id = "\x63\x72\151\x74\x69\143\141\154\x2d\143\x73\x73"; goto qLAnY; ybJLE: parent::__construct(); goto doCw6; qLAnY: $this->paths = [Util::getFullPath()]; goto ybJLE; doCw6: } }
+ namespace Optimization\Critical\CSS; use Optimization\Critical\Purge as BaseClass; class Purge extends BaseClass { public function __construct() { goto p_e51; p_e51: $this->id = "\143\162\151\164\151\x63\x61\x6c\x2d\x63\x73\163"; goto Ti_uT; Ti_uT: $this->paths = [Util::getFullPath()]; goto BV_23; BV_23: parent::__construct(); goto r3NzH; r3NzH: } }
--- src/Optimization/Purge/Common.php
+++ src/Optimization/Purge/Common.php
@@ -1,2 +1,2 @@
 <?php
- namespace Optimization\Purge; use Optimization\Common as BaseClass; class Common extends BaseClass { protected ?string $path; public function __construct(?string $path) { $this->path = $path; parent::__construct(); } }
+ namespace Optimization\Purge; use Optimization\Common as BaseClass; class Common extends BaseClass { protected ?string $path; public function __construct(?string $GuTuU) { $this->path = $GuTuU; parent::__construct(); } }
